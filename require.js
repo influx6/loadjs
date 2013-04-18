9923aecaa9272d13d055eec2ea5c94baa42f651c
Ewetumo Alexander <trinoxf@gmail.com>
2013-04-18 21:29:32 +0100

    testing directory finder

--- require.js
+++ require.js
@@ -1,38 +1,55 @@
-var isReady = false,
-	isCEM = (typeof window !== 'undefined' && navigator && document),
-	isOpera = (typeof opera !== 'undefined' && opera.toString() === "[object Opera]"),
-	isBrowser = (isCEM || isOpera),
-	isNode = (function(){
-		return (
-			(typeof process !== 'undefined' && typeof module !== 'undefined') 
-				&& (process['title'] === 'node' && typeof module['exports'] !== 'undefined')
-				&& (typeof process['execPath'] !== 'undefined' && process['execPath'].match(/node$/))
-			);
-	})(),
-	defProperty = function(obj,name,fns,properties){
-      if(!obj || !name || typeof fns !== 'object')) return; 
-      
-      if(!fns['get']) fns.get = function(){};
-      if(!fns['set']) fns.set = function(){};
+(function(global){
 
-       if(!("defineProperty" in Object) && Object.__defineGetter__ && Object.__defineSetter__){
-        if(fns.get) obj.__defineGetter__(name,fns.get);
-        if(fns.set) obj.__defineSetter__(name,fns.set);
-        if(properties) obj.defineProperty(name,properties);
-        return;
-      }
+	var isReady = false,
+		isCEM = (typeof window !== 'undefined' && !!navigator && !!document),
+		isOpera = (typeof opera !== 'undefined' && opera.toString() === "[object Opera]"),
+		isBrowser = (isCEM || isOpera),
+		isNode = (function(){
+			return (
+				(typeof process !== 'undefined' && typeof module !== 'undefined') 
+					&& (process['title'] === 'node' && typeof module['exports'] !== 'undefined')
+					&& (typeof process['execPath'] !== 'undefined' && !!process['execPath'].match(/node$/))
+				);
+		})(),
+		defProperty = function(obj,name,fns,properties){
+	      if(!obj || !name || typeof fns !== 'object') return; 
+	      
+	      if(!fns['get']) fns.get = function(){};
+	      if(!fns['set']) fns.set = function(){};
 
-      return Object.defineProperty(obj,name,{
-        get: fns.get, set: fns.set
-      },properties);
+	       if(!("defineProperty" in Object) && Object.__defineGetter__ && Object.__defineSetter__){
+	        if(fns.get) obj.__defineGetter__(name,fns.get);
+	        if(fns.set) obj.__defineSetter__(name,fns.set);
+	        if(properties) obj.defineProperty(name,properties);
+	        return;
+	      }
 
-    },
-    arr = Array.prototype,
-    obj = Object.prototype,
-    exposed = {
-		exports : null,
-		require : function(package){},
-    };
+	      return Object.defineProperty(obj,name,{
+	        get: fns.get, set: fns.set
+	      },properties);
 
+	    },
+	    paths = [
+	    	'./assets/js',
+		    './modules/js',
+		    './modules',
+		    './assets',
+		    './'
+	    ],
+	    arr = Array.prototype,
+	    obj = Object.prototype,
+	    e = {
+			exports : null,
+			require : function(package){},
+			loaded: {},
+			paths: paths
+	    };
 
- //expose require and exports to the environments
+	 //expose require and exports to the environments
+	 if(!isNode && isBrowser){	
+	 	global.module = e;
+	 	global.exports = e.exports; 
+	 	global.require = e.require; 
+	 };
+
+})(this);
